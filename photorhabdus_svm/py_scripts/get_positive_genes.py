22fb5dce3537e73d04b9a3e90e4a31324abe4adc
Desiree Ho <desireeho@berkeley.edu>
2020-08-30 04:08:25 -0700

    Oversampling hypothetical effectors
    Instead of arbitrarily picking one representative gene for each orthogroup, using all outputs from OrthoFinder
    effectively going from 40 to 682 genes in the positive set

---
+++ photorhabdus_svm/py_scripts/get_positive_genes.py
@@ -0,0 +1,3 @@
+import pandas as pd 
+
+print(pd.read_pickle('/Library/Photorhabdus/unzip/pickled_effector'))
---
+++ photorhabdus_svm/py_scripts/get_positive_sets.py
@@ -0,0 +1,42 @@
+import os
+import csv
+import random
+import pandas as pd 
+from Bio import SeqIO
+
+'''MAKE A 2D LIST CONTAINING SEPARATED LOCI FOR GENES IN THE ORTHOGROUP. EXPORT GB FILE FROM ASSEMBLY.'''
+
+orthofinder = '/Users/desho/Desktop/photorhabdus_svm/positives_intermediate/orthofinder_output.csv'
+photorhabdus = '/Library/Photorhabdus/unzip'
+os.chdir(photorhabdus)
+
+df = pd.read_csv(orthofinder)
+
+dfb = pd.DataFrame()
+i = 0
+
+effector = [[] for i in range(40)]
+
+for column in df:
+    df.rename(columns = {column:column[:-2]}, inplace = True)
+
+def collect_genes(gb):
+    global effector
+    seq_record = SeqIO.parse(gb, "genbank")
+    for record in seq_record:
+        if record.name in df:
+            print(record.name)
+            orthogroups = list(df[record.name])
+            for x in range(40):
+                if type(orthogroups[x]) == str:
+                    effector[x] += orthogroups[x].split(',')
+            
+            with open(record.name + '.gb', 'w') as output_handle:
+                SeqIO.write(record, output_handle, 'fasta')
+
+for gb in os.listdir(photorhabdus):
+    if 'gbff' in gb:
+        collect_genes(gb)
+
+df = pd.DataFrame(effector)
+df.to_pickle('pickled_effector')
